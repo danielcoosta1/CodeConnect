82c4a9461cbcef5ef6467c8d8994959e84b712aa
Daniel Costa <daniel.dc697@gmail.com>
2025-10-14 13:36:07 -0300

    Adicionando o axios.defaults para repasasr o token para todas as requisições futuras da aplicação

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -3,13 +3,17 @@ import { AuthContext } from "./AuthContext";
 import axios from "axios";
 import { localStorageService } from "../services/localStorageService";
 
+
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [token, setToken] = useState(null);
-  
+
+  //Restaura o estado de autenticação ao carregar o aplicativo
   useEffect(() => {
     const storedToken = localStorageService.ler("user");
     const storedUser = localStorageService.ler("token");
+    // ATIVA O CRACHÁ ao restaurar a sessão
+    axios.defaults.headers.common["Authorization"] = `Bearer ${storedToken}`;
     if (storedToken && storedUser) {
       setToken(storedToken);
       setUser(storedUser);
@@ -24,6 +28,8 @@ const AuthProvider = ({ children }) => {
       const { user: userData, token: authToken } = response.data;
       localStorageService.salvar("token", authToken);
       localStorageService.salvar("user", userData);
+      // ATIVA O CRACHÁ após o login
+      axios.defaults.headers.common["Authorization"] = `Bearer ${authToken}`;
       setUser(userData);
       setToken(authToken);
     } catch (error) {
@@ -35,12 +41,16 @@ const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorageService.remover("token");
     localStorageService.remover("user");
+    // REMOVE O CRACHÁ no logout
+    delete axios.defaults.headers.common["Authorization"];
     setToken(null);
     setUser(null);
   };
 
   return (
-    <AuthContext.Provider value={{ user, token, login, logout }}>
+    <AuthContext.Provider
+      value={{ user, token, login, logout, isAuthenticated: !!token }}
+    >
       {children}
     </AuthContext.Provider>
   );
