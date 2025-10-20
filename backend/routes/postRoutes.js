860822b4795c0ce666edfdf934bffd4495046cd2
Daniel Costa <daniel.dc697@gmail.com>
2025-10-20 08:52:15 -0300

    Chamada da rota createPost

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { getAllPosts } from "../controllers/postController.js";
-
+import { createPost, getAllPosts } from "../controllers/postController.js";
+import { authMiddleware } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 // Rota para buscar/listar todos os posts
 router.get('/', getAllPosts);
+
+// Rota para criar um novo post
+router.post('/', authMiddleware, createPost);
+
 
 export default router;
