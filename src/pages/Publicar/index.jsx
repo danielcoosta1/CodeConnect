bee02716aa5492f542576e8743818cc1fdd15d78
Daniel Costa <daniel.dc697@gmail.com>
2025-10-22 08:42:59 -0300

    Lidando com o Upload ta imagem

--- src/pages/Publicar/index.jsx
+++ src/pages/Publicar/index.jsx
@@ -17,7 +17,7 @@ import {
 
 import { FaTrash } from "react-icons/fa";
 import { MdPublish } from "react-icons/md";
-import exemploImg from "./assets/exemplo.png";
+import defaultImg from "./assets/exemplo.png";
 
 import { useRef, useState } from "react";
 import axios from "axios";
@@ -35,6 +35,33 @@ const Publicar = () => {
 
   const [erro, setErro] = useState("");
   const [loading, setLoading] = useState(false);
+
+  const lidarComUpload = (event) => {
+    const file = event.target.files[0];
+    if (!file) return;
+
+    const tiposAceitos = ["image/jpeg", "image/png", "image/gif"];
+    if (!tiposAceitos.includes(file.type)) {
+      setErro("Tipo de arquivo não suportado. Por favor, envie uma imagem.");
+      return;
+    }
+
+    if (file.size > 5 * 1024 * 1024) {
+      setErro("O tamanho do arquivo excede o limite de 5MB.");
+      return;
+    }
+
+    const reader = new FileReader(); // Cria um leitor de arquivos
+
+    // Quando o arquivo for carregado
+    reader.onload = () => { 
+      const base64String = reader.result.split(",")[1]; // Extrai a parte base64 que seria o  src da imagem
+      setImage(base64String);// Armazena a imagem em base64 no estado
+      setImageFileName(file.name); // Armazena o nome do arquivo no estado
+    };
+
+    reader.readAsDataURL(file); // Converte o arquivo para base64
+  };
 
   const limparCampos = () => {
     setTitle("");
@@ -70,10 +97,19 @@ const Publicar = () => {
     <ContainerWrapper>
       <ContainerUploadImg>
         <ContainerImg>
-          <Img src={exemploImg} alt="Imagem de exemplo" />
+          <Img src={defaultImg} alt="Imagem de exemplo" />
         </ContainerImg>
         <ContainerButton>
-          <ButtonUploadImg>Carregar Imagem</ButtonUploadImg>
+          <ButtonUploadImg onClick={() => inputRef.current.click()}>
+            <p>Carregar Imagem </p>
+            <input
+              type="file"
+              ref={inputRef}
+              accept="image/"
+              onChange={lidarComUpload}
+              style={{ display: "none" }}
+            />
+          </ButtonUploadImg>
           <LegendaImg>Legenda da Imagem</LegendaImg>
         </ContainerButton>
       </ContainerUploadImg>
