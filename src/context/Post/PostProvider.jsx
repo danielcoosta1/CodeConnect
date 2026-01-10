c9325d91acdfbee5af10ce3c06a65fc4f35f00ad
Daniel Costa <daniel.dc697@gmail.com>
2026-01-10 17:23:31 -0300

    Criando as primeiras logicas do provider

--- src/context/Post/PostProvider.jsx
+++ src/context/Post/PostProvider.jsx
@@ -1,7 +1,52 @@
+import { useReducer } from "react";
 import { PostContext } from "./PostContext";
+import { postReducer } from "./postReducer";
+import { initialState } from "./initialState";
 
-const PostProvider = ({ children }) => {
-  return <PostContext.Provider value={{}}> {children} </PostContext.Provider>;
+export const PostProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(postReducer, initialState);
+
+  const atualizarDato = (field, value) => {
+    dispatch({ type: "SET_DADO", field, payload: value });
+  };
+
+  const adicionarTag = (novaTag) => {
+    dispatch({ type: "ADD_TAG", payload: novaTag });
+  };
+
+  const removerTag = (tag) => {
+    dispatch({ type: "REMOVE_TAG", payload: tag });
+  };
+
+  const removerImagem = () => {
+    dispatch({ type: "REMOVER_IMAGEM" });
+  };
+
+  const limparCampos = () => {
+    dispatch({ type: "LIMPAR_CAMPOS" });
+  };
+
+  return (
+    <PostContext.Provider
+      value={{
+        title: state.title,
+        content: state.content,
+        tags: state.tags,
+        tagInput: state.tagInput,
+        image: state.image,
+        imageFileName: state.imageFileName,
+        loading: state.loading,
+        error: state.error,
+        success: state.success,
+        atualizarDato,
+        adicionarTag,
+        removerTag,
+        removerImagem,
+        limparCampos,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </PostContext.Provider>
+  );
 };
-
-export default PostProvider;
