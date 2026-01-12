119d98ef62ca6b832b1ea91f1f76fee4687920fc
Daniel Costa <daniel.dc697@gmail.com>
2026-01-12 09:55:51 -0300

    Adicionando novas manipulações de estado e novas integrações de API - Publicar projeto

--- src/context/Post/PostProvider.jsx
+++ src/context/Post/PostProvider.jsx
@@ -2,9 +2,13 @@ import { useReducer } from "react";
 import { PostContext } from "./PostContext";
 import { postReducer } from "./postReducer";
 import { initialState } from "./initialState";
+import { toastErro, toastSucesso } from "../../utils/toast";
+import { createPostRequest } from "./postService";
 
 export const PostProvider = ({ children }) => {
   const [state, dispatch] = useReducer(postReducer, initialState);
+
+  //  -------  AÇÕES DE MANIPULAÇÃO DDO ESTADO LOCAL   -------- 
 
   const atualizarDato = (field, value) => {
     dispatch({ type: "SET_DADO", field, payload: value });
@@ -18,12 +22,55 @@ export const PostProvider = ({ children }) => {
     dispatch({ type: "REMOVE_TAG", payload: tag });
   };
 
+  const definirImagem = (image, fileName) => {
+    dispatch({ type: "SET_IMAGE", payload: { image, fileName } });
+  };
+
   const removerImagem = () => {
     dispatch({ type: "REMOVER_IMAGEM" });
   };
 
-  const limparCampos = () => {
+  const limparFormulario = () => {
     dispatch({ type: "LIMPAR_CAMPOS" });
+  };
+
+  // -------  AÇÕES DE INTEGRAÇÃO COM A API   --------
+
+
+  const publicarPost = async () => {
+    // VALIDAÇÃO BÁSICA ANTES DE ENVIAR
+    if(state.title.trim().length < 3) {
+      toastErro("O título precisa ter pelo menos 3 caracteres.");
+      return;
+    }
+
+    if(state.content.trim().length < 10) {
+      toastErro("O conteúdo precisa ter pelo menos 10 caracteres.");
+      return;
+    }
+
+    dispatch({ type: "INICIAR_PUBLICACAO" });
+    try {
+
+      const postData = {
+        title: state.title,
+        content: state.content,
+        image: state.image,
+        imageFileName: state.imageFileName,
+        tags: state.tags,
+      };
+
+      await createPostRequest(postData);
+
+      dispatch({ type: "PUBLICACAO_SUCESSO" });
+      toastSucesso("Post publicado com sucesso!");
+
+      setTimeout(() => limparFormulario(), 1500); // Limpa após 1.5s para ver o sucesso
+   
+    } catch (error) {
+      dispatch({ type: "PUBLICACAO_ERRO", payload: error.message });
+      toastErro("Erro ao publicar o post.");  
+    }
   };
 
   return (
@@ -41,8 +88,10 @@ export const PostProvider = ({ children }) => {
         atualizarDato,
         adicionarTag,
         removerTag,
+        definirImagem,
         removerImagem,
-        limparCampos,
+        limparFormulario,
+        publicarPost
       }}
     >
       {" "}
