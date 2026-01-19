e38b0c3bab1813fcbd3e12abc6b7e8968ed72729
Daniel Costa <daniel.dc697@gmail.com>
2026-01-19 07:49:07 -0300

    Adicionando o globalStyle - não havia feito até entao

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppContent from "./components/AppContent";
 import AuthProvider from "./context/Auth/AuthProvider";
 import { PostProvider } from "./context/Post/PostProvider";
+import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <AuthProvider>
         <PostProvider>
           <AppContent />
+          <GlobalStyle />
         </PostProvider>
       </AuthProvider>
     </Router>
