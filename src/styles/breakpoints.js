cde681c9fc6b7832d2742eccb099b8e2ca3299b8
Daniel Costa <daniel.dc697@gmail.com>
2026-02-09 11:24:50 -0300

    Criando o arquivo para unificar os devices na responsivdade da página

---
+++ src/styles/breakpoints.js
@@ -0,0 +1,13 @@
+export const breakpoints = {
+  mobile: "768px",
+  tablet: "1024px",
+  laptop: "1280px",
+  desktop: "1440px",
+};
+
+export const device = {
+  mobile: `(max-width: ${breakpoints.mobile})`,
+  tablet: `(max-width: ${breakpoints.tablet})`,
+  laptop: `(max-width: ${breakpoints.laptop})`,
+  desktop: `(max-width: ${breakpoints.desktop})`,
+};
