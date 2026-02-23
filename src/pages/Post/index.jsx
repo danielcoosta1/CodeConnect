5f13033128a2c411b77b7aaae0a46034a5ea6707
Daniel Costa <daniel.dc697@gmail.com>
2026-02-23 11:11:31 -0300

    feat(Post): adicionando o ModalConfirmacao para esse contexto e adicionando a Guard Clause no componente

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 import ProfileAvatar from "../../components/ProfileAvatar";
@@ -34,16 +34,20 @@ import {
 import { usePost } from "../../hooks/usePost";
 import { useAuth } from "../../hooks/useAuth";
 import { FaCode, FaRegComment } from "react-icons/fa";
+import ModalConfirmacao from "../../components/ModalConfirmacao";
 
 const Post = () => {
   const { id } = useParams(); // Pega o ID da URL
   const navigate = useNavigate();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const {
     postDetails,
     loadingPostDetails,
     errorPostDetails,
     carregarPostPorId,
+    deletarPostPorId,
+    loadingDeletePost,
   } = usePost();
 
   const { user } = useAuth();
@@ -55,6 +59,15 @@ const Post = () => {
       carregarPostPorId(id);
     }
   }, [id]);
+
+  const handleConfirmarExclusao = async () => {
+    const sucesso = await deletarPostPorId(id);
+
+    // Se deu certo, ele manda pro feed. (O modal já se fechou sozinho!)
+    if (sucesso) {
+      navigate("/feed");
+    }
+  };
 
   if (loadingPostDetails) {
     return <LoadingState texto="Carregando post..." size={45} />;
@@ -69,128 +82,144 @@ const Post = () => {
     );
   }
 
+  if (!postDetails) {
+    return null;
+  }
+
   // Renderização de Sucesso
   return (
-    <PostContainer>
-      <PostHeader>
-        <HeaderTop>
-          <BackButton onClick={() => navigate(-1)}>
-            <FaArrowLeft /> Voltar
-          </BackButton>
+    <>
+      <PostContainer>
+        <PostHeader>
+          <HeaderTop>
+            <BackButton onClick={() => navigate(-1)}>
+              <FaArrowLeft /> Voltar
+            </BackButton>
 
-          {/* Renderização Condicional: Só aparece se for o dono! */}
-          {isAuthor && (
-            <AuthorActions>
-              <button
-                className="btn-edit"
-                onClick={() => console.log("Editar post")}
-              >
-                <FaRegEdit /> <span>Editar</span>
-              </button>
-              <button
-                className="btn-delete"
-                onClick={() => console.log("Excluir post")}
-              >
-                <FaTrash /> <span>Excluir</span>
-              </button>
-            </AuthorActions>
-          )}
-        </HeaderTop>
+            {/* Renderização Condicional: Só aparece se for o dono! */}
+            {isAuthor && (
+              <AuthorActions>
+                <button
+                  className="btn-edit"
+                  onClick={() => console.log("Editar post")}
+                >
+                  <FaRegEdit /> <span>Editar</span>
+                </button>
+                <button
+                  className="btn-delete"
+                  onClick={() => setIsModalOpen(true)}
+                >
+                  <FaTrash /> <span>Excluir</span>
+                </button>
+              </AuthorActions>
+            )}
+          </HeaderTop>
 
-        <CoverImage>
-          <img
-            src={
-              postDetails.image
-                ? `data:image/png;base64,${postDetails.image}`
-                : defaultImg
-            }
-            alt={`Capa do projeto ${postDetails.title}`}
-          />
-        </CoverImage>
-
-        <PostTitle>{postDetails.title}</PostTitle>
-        <LinksContainer>
-          <ProjectLink
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-            $primary={true}
-          >
-            <FaExternalLinkAlt /> <span>Acessar Projeto</span>
-          </ProjectLink>
-
-          <ProjectLink href="#" target="_blank" rel="noopener noreferrer">
-            <FaGithub /> <span>Ver Código-Fonte</span>
-          </ProjectLink>
-        </LinksContainer>
-        <PostContent>
-          <p>{postDetails.content}</p>
-        </PostContent>
-
-        {postDetails.tags && postDetails.tags.length > 0 && (
-          <TagList>
-            {postDetails.tags.map((tag, index) => (
-              <TagItem key={index}>{tag}</TagItem>
-            ))}
-          </TagList>
-        )}
-
-        <AuthorContainer>
-          <ActionIcons>
-            <IconGroup>
-              <FaCode /> <span>0</span>
-            </IconGroup>
-            <IconGroup>
-              <FaShareNodes /> <span>0</span>
-            </IconGroup>
-            <IconGroup>
-              <FaRegComment /> <span>0</span>
-            </IconGroup>
-          </ActionIcons>
-          <AuthorInfo to={`/perfil/${postDetails.author.id}`}>
-            <ProfileAvatar
-              src={postDetails.author?.imagem}
-              size={50}
-              hasBorder={true}
+          <CoverImage>
+            <img
+              src={
+                postDetails.image
+                  ? `data:image/png;base64,${postDetails.image}`
+                  : defaultImg
+              }
+              alt={`Capa do projeto ${postDetails.title}`}
             />
-            <div>
-              <h3>
-                {postDetails.author.nome} {postDetails.author.sobrenome}
-              </h3>
-              <small>@{postDetails.author.usuario}</small>
-            </div>
-          </AuthorInfo>
-        </AuthorContainer>
-      </PostHeader>
-      <CodeContainer>
-        <pre>
-          <code>
-            {`// Trecho de código em destaque
+          </CoverImage>
+
+          <PostTitle>{postDetails.title}</PostTitle>
+          <LinksContainer>
+            <ProjectLink
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+              $primary={true}
+            >
+              <FaExternalLinkAlt /> <span>Acessar Projeto</span>
+            </ProjectLink>
+
+            <ProjectLink href="#" target="_blank" rel="noopener noreferrer">
+              <FaGithub /> <span>Ver Código-Fonte</span>
+            </ProjectLink>
+          </LinksContainer>
+          <PostContent>
+            <p>{postDetails.content}</p>
+          </PostContent>
+
+          {postDetails.tags && postDetails.tags.length > 0 && (
+            <TagList>
+              {postDetails.tags.map((tag, index) => (
+                <TagItem key={index}>{tag}</TagItem>
+              ))}
+            </TagList>
+          )}
+
+          <AuthorContainer>
+            <ActionIcons>
+              <IconGroup>
+                <FaCode /> <span>0</span>
+              </IconGroup>
+              <IconGroup>
+                <FaShareNodes /> <span>0</span>
+              </IconGroup>
+              <IconGroup>
+                <FaRegComment /> <span>0</span>
+              </IconGroup>
+            </ActionIcons>
+            <AuthorInfo to={`/perfil/${postDetails.author.id}`}>
+              <ProfileAvatar
+                src={postDetails.author?.imagem}
+                size={50}
+                hasBorder={true}
+              />
+              <div>
+                <h3>
+                  {postDetails.author.nome} {postDetails.author.sobrenome}
+                </h3>
+                <small>@{postDetails.author.usuario}</small>
+              </div>
+            </AuthorInfo>
+          </AuthorContainer>
+        </PostHeader>
+        <CodeContainer>
+          <pre>
+            <code>
+              {`// Trecho de código em destaque
 function saudarComunidade(usuario) {
   console.log(\`Olá, \${usuario}! Bem-vindo ao meu projeto.\`);
   return true;
 }
 
 saudarComunidade("Dev");`}
-          </code>
-        </pre>
-      </CodeContainer>
-      <CommentsContainer>
-        <h2>Comentários</h2>
-        <p>Seja o primeiro a comentar neste projeto!</p>
+            </code>
+          </pre>
+        </CodeContainer>
+        <CommentsContainer>
+          <h2>Comentários</h2>
+          <p>Seja o primeiro a comentar neste projeto!</p>
 
-        <FakeInputComment>
-          <input
-            type="text"
-            placeholder="Adicione um comentário..."
-            disabled={loadingPostDetails}
-          />
-          <button type="button">Comentar</button>
-        </FakeInputComment>
+          <FakeInputComment>
+            <input
+              type="text"
+              placeholder="Adicione um comentário..."
+              disabled={loadingPostDetails}
+            />
+            <button type="button">Comentar</button>
+          </FakeInputComment>
 
-        {/* Futuramente faremos um map() dos comentários reais aqui */}
-      </CommentsContainer>
-    </PostContainer>
+          {/* Futuramente faremos um map() dos comentários reais aqui */}
+        </CommentsContainer>
+      </PostContainer>
+
+      <ModalConfirmacao
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleConfirmarExclusao}
+        titulo="Excluir Projeto"
+        mensagem="Tem certeza de que deseja excluir este projeto? Esta ação não poderá ser desfeita."
+        textoConfirmar="Sim, Excluir"
+        isDestructive={true} // Ativa o botão vermelho!
+      />
+    </>
   );
 };
 
