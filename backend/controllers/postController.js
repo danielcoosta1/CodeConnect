0a817e332b75611af23674498b5d1d55df3293eb
Daniel Costa <daniel.dc697@gmail.com>
2026-03-14 15:58:04 -0300

    fix(Post): adicionamos o include(author) na rota POST para a nova pubicação - evitação os dados undefined ao abrir o feed

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -69,6 +69,9 @@ export const createPost = async (req, res) => {
           connect: { id: authorId },
         },
       },
+      include: {
+        author: true,
+      },
     });
     res.status(201).json(newPost);
   } catch (error) {
